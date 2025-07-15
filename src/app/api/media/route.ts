403c2a881839782e7f5379aab83a510bd5984b0b
anouckce <75619667+anouckn@users.noreply.github.com>
2025-07-15 17:41:10 +0400

    Update route.ts

--- src/app/api/media/route.ts
+++ src/app/api/media/route.ts
@@ -1,20 +1,11 @@
 import { getMediaPosts } from "@/lib/notion";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
-export async function GET(request: NextRequest) {
-  const { searchParams } = new URL(request.url);
-  const databaseId = searchParams.get("db");
-  const token = searchParams.get("token");
-
-  if (!databaseId || !token) {
-    return NextResponse.json(
-      { error: "Missing database ID or token" },
-      { status: 400 }
-    );
-  }
-
+export async function GET(request: Request) {
   try {
-    const posts = await getMediaPosts(databaseId, token);
+    console.log("request", request.headers.get("cache-control"));
+    const posts = await getMediaPosts(request); // <-- fixed line
+    console.log({ posts });
     const response = NextResponse.json(posts);
     response.headers.set("Cache-Control", "no-store");
     return response;
